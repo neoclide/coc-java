54ffee9375d348da0d5e5290aa744496bb21e409
chemzqm <chemzqm@gmail.com>
2019-06-18 05:19:50 +0800

    use webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 const path = require('path')
 
 module.exports = {
-  entry: './lib/index',
+  entry: './src/index.ts',
   target: 'node',
   mode: 'none',
   resolve: {
     mainFields: ['module', 'main'],
-    extensions: ['.js']
+    extensions: ['.js', '.ts']
   },
   externals: {
     'coc.nvim': 'commonjs coc.nvim'
   },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'ts-loader',
+        options: {
+          compilerOptions: {
+            "sourceMap": true,
+          }
+        }
+      }]
+    }]
+  },
   output: {
-    path: path.resolve(__dirname, '.release/lib'),
+    path: path.join(__dirname, 'lib'),
     filename: 'index.js',
     libraryTarget: "commonjs",
   },
   plugins: [
   ],
   node: {
-    __dirname: false
+    __dirname: false,
+    __filename: false
   }
 }
+
